3bbb739bea9fb7a6b93c5518e462f5437feeeb42
Velidoss <velidoss11@gmail.com>
2020-10-13 20:59:01 +0300

    transform-array in process...

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,7 +1,6 @@
-const CustomError = require("../extensions/custom-error");
+
 module.exports = function transform( arr ) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
   if (Array.isArray(arr)) {
     let result = [...arr]
     for (let i = 0; i < result.length; i++) {
