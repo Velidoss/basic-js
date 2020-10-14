c6a68c33cfefbf7540ea8432a49ce3f6c3f4fd5b
Velidoss <velidoss11@gmail.com>
2020-10-14 14:37:44 +0300

    feat: finished basic js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,38 +1,42 @@
-const CustomError = require("../extensions/custom-error");
+
 module.exports = function transform( arr ) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-  if (Array.isArray(arr)) {
-    let result = [...arr]
-    for (let i = 0; i < result.length; i++) {
-      switch (result[i]) {
-        case '--discard-next':
-          result.splice(i+1, 1)
-          break
-        case '--discard-prev':
-          if(result[i-1] !== '--discard-next' && result[i-1] !== '--discard-prev' && result[i-1] !== '--double-next' && result[i-1] !== '--double-prev'){
-            result.splice(i-1, 1)
-            break
-          }
-          break
-        case '--double-next':
-          result.splice(i+1, 0, result[i+1])
-          break
-        case '--double-prev':
-          if(result[i-1] !== '--discard-next' && result[i-1] !== '--discard-prev' && result[i-1] !== '--double-next' && result[i-1] !== '--double-prev'){
-            result.splice(i-1, 0, result[i-1])
-            break
-          }
-          break
-        default:
-          break
-      }
+  if (!Array.isArray(arr)) throw new Error('arr has to be array!')
+  let arrCopy = [...arr]
+  let res = []
+  for (let i = 0; i < arrCopy.length; i++) {
+    switch (arrCopy[i]) {
+      case '--discard-prev':
+        res.pop()
+        res.push(arrCopy[i])
+        break
+      case '--discard-next':
+        arrCopy.splice(i + 1, 1)
+        res.push(arrCopy[i])
+        break
+      case '--double-prev':
+        if(res.length>0){
+          res.push(res[res.length-1])
+        }
+        res.push(arrCopy[i])
+        break
+      case '--double-next':
+        if (i < arrCopy.length - 1) {
+          arrCopy.splice(i + 1, 0, arrCopy[i + 1])
+        }
+        res.push(arrCopy[i])
+        break
+      default:
+        res.push(arrCopy[i])
+        break
     }
-    return result.filter((elem)=> {
-      if (elem !== '--discard-next' && elem !== '--discard-prev' && elem !== '--double-next' && elem !== '--double-prev'){
-        return elem
-      }})
-  } else {
-    throw new Error()
   }
-};
+  let answer = []
+  for (let i = 0; i < res.length; i++) {
+    if (res[i] === '--discard-prev' || res[i] === '--discard-next' || res[i] === '--double-prev' || res[i] === '--double-next' || res[i] === undefined) {
+      continue
+    } else {
+      answer.push(res[i])
+    }
+  }
+  return answer
+}
